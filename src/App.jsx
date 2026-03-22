cf6667f55b693790178e61d7e819f16485dfd6a8
Rushie <hirapararushi33@gmail.com>
2026-03-22 15:11:44 +0530

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Lenis from 'lenis';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
